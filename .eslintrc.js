d5b0bca5bdf33cba7a235d93ce8439f716a59936
Nejdet Dalaslan <76647121+aiehtshw@users.noreply.github.com>
2024-08-25 22:43:26 +0300

    eslint error was fixed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   root: true,
-  extends: ['universe', 'universe/native'],
+  extends: '@react-native',
   rules: {
-    'import/order': ['warn', {}],
     'react-native/no-inline-styles': 0,
     'react-hooks/exhaustive-deps': 'off',
     '@typescript-eslint/no-shadow': 'off',
