55da0a1e3ae62bdb60a191db3ace9a88d6b02c10
Nejdet Dalaslan <76647121+aiehtshw@users.noreply.github.com>
2024-08-25 23:01:00 +0300

    utils folder was created
    utils folder was created
    unnecessary comment was removed from App.tsx

---
+++ src/utils/languages/locales/tr.ts
@@ -0,0 +1,7 @@
+/**
+ * TODO: If localization was wanted, tr.ts folder will copy, translate and paste to this page
+ * After that write a redux slice and connect tr.ts and en.ts to one state.
+ * */
+export default {
+  language: 'tr',
+};
