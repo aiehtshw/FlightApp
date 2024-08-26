cc6c3fdc020149a2931461c5e6f729201e29153a
Nejdet Dalaslan <76647121+aiehtshw@users.noreply.github.com>
2024-08-26 16:27:20 +0300

    background component was created and integrated to screens

--- src/screens/flightsTabScreens/dashboard/Dashboard.tsx
+++ src/screens/flightsTabScreens/dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
+import Background from '../../../components/background';
 
 const Dashboard = () => {
   return (
-    <View>
+    <Background>
       <Text>Hello</Text>
-    </View>
+    </Background>
   );
 };
 
--- src/screens/plansTabScreens/plans/Plans.tsx
+++ src/screens/plansTabScreens/plans/Plans.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text} from 'react-native';
+import Background from '../../../components/background';
 
 const Plans = () => {
   return (
-    <View>
+    <Background>
       <Text>Hello</Text>
-    </View>
+    </Background>
   );
 };
 
